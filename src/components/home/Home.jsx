876e8c3733e0c8b70f61e39fac3e2c0dce775616
HzamiBaha <bahaeddine.hzami@gmail.com>
2022-10-12 16:31:58 +0100

    responsive home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./home.css"
 import Social from './Social'
 import Data from './Data'
+import ScrollDown from './ScrollDown'
 const Home = () => {
     return (
         <section className="home section" id="home">
@@ -11,6 +12,7 @@ const Home = () => {
                     <div className="home__img"></div>
                     <Data/>
                 </div>
+                <ScrollDown/>
             </div>
         </section>
     )
