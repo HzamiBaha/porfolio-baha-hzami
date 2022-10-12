0afbd871ddd7897c4b442cf00f9c19ac8d1d3621
HzamiBaha <bahaeddine.hzami@gmail.com>
2022-10-12 13:25:11 +0100

    Home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
+import Home from './components/home/Home'
 function App() {
 
 
   return (
     <>
-      <Header></Header>
+      <Header/>
+      <main className='main'>
+        <Home/>
+      </main>
     </>
   )
 }
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://github.com/HzamiBaha" className="home__social-icon" target="_blank">
+                <i class="uil uil-github-alt"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/baha-eddine-hzami/" className="home__social-icon" target="_blank">
+                <i class="uil uil-linkedin-alt"></i>
+            </a>
+            <a href="" className="home__social-icon" target="_blank">
+                <i class="uil uil-medium-m"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
