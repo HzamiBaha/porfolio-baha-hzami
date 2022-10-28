a2fb779b414c972aee247574b4b79360d3c478e1
HzamiBaha <bahaeddine.hzami@gmail.com>
2022-10-28 16:08:22 +0100

    resume update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/Resume Baha Hzami.pdf";
+import CV from "../../assets/Baha_Hzami_Resume.pdf";
 import Info from "./Info";
 
 const About = () => {
